1c16317ec8a76bb7ad5235f7f8627ef876ac0f20
vlad <xergut@gmail.com>
2021-12-09 00:13:58 +0300

    more more more

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -2,63 +2,86 @@ import React, { useState, useRef, useEffect } from "react";
 import { YMaps, Map, Placemark } from 'react-yandex-maps';
 
 
-function  MapComponent (){
+function MapComponent() {
 
-  const [markers, setMarker] = useState([]);
-  const mapRef = useRef(null);
-  const [value, setValue] = useState('');
+    const [markers, setMarker] = useState([]);
+    const mapRef = useRef(null);
+    const markerRef = useRef(null)
+    const [value, setValue] = useState('');
+    const [id, setID] = useState(0)
 
-  const Cons = (event) => {
-      event.preventDefault()
-      const center = mapRef.current.getCenter()
-      console.log(center, value)
-      setMarker([...markers, center])
-      console.log(markers)
-  }
+    const Cons = (event) => {
+        event.preventDefault()
+        try {
+            if (value) {
+                const center = mapRef.current.getCenter()
+                const marker = {
+                    title: value,
+                    latlng: center,
+                    idx: id
+                }
+                setMarker([...markers, marker])
+                setValue('');
+                setID(id+1)
+            }
+        } catch (e) {
 
-  const onValueChange = (event) => {
-    setValue(event.target.value);
-  }
+        }
+
+    }
+
+    const onDragEnded = () => {
+        console.log(markerRef.current.geometry._coordinates)
+    }
+
+    const onValueChange = (event) => {
+        setValue(event.target.value);
+    }
 
     return (
-        <div width="800px">
-        <YMaps>
-          My awesome application with maps!
-          <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} 
-                width="100%"
-                instanceRef={ref => (mapRef.current = ref)} >
-                {markers.map(marker =>
-                    <Placemark geometry={marker}
-                                properties={{
-                                    hintContent: 'Stack Overflow',
-                                    balloonContent: 'Stack Overflow на русском',
-                                    
-                                }}
-                                modules={
-                                    ['geoObject.addon.balloon', 'geoObject.addon.hint']
-                                }/>)
+        <div>
+            <YMaps>
+                <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }}
+                    width="100%"
+                    instanceRef={ref => (mapRef.current = ref)} >
+                    {markers.map(marker =>
+                        <Placemark geometry={marker.latlng} 
+                            key = {marker.idx}
+                            instanceRef = {ref => (markerRef.current = ref)}
+                            properties={{
+                                hintContent: marker.title,
+                                balloonContent: 'Stack Overflow на русском',
+
+                            }}
+                            modules={
+                                ['geoObject.addon.balloon', 'geoObject.addon.hint']
+                            }
+                            options={{
+                                draggable: true
+                            }} 
+                            onDragEnd = {onDragEnded}/>)
                     }
-          </Map>
-      </YMaps>
-      <div className="add-form">
-      <h4>Введите название маркера</h4>
-      <form onSubmit = {Cons}>
-        <input 
-          className="form-control"
-          type="text" 
-          value={value}
-          onChange={onValueChange}/>
-      </form>
-        </div>
-            <button onClick = {Cons}></button>
+                </Map>
+            </YMaps>
+            <div className="add-form">
+                <h4>Введите название маркера</h4>
+                <form onSubmit={Cons}>
+                    <input
+                        className="form-control"
+                        type="text"
+                        value={value}
+                        onChange={onValueChange} />
+                </form>
+            </div>
+            <button onClick={Cons}></button>
             <ul className="list-group">
-        {markers.map(marker=> 
-            <li>
-                <div>{marker[0]}</div>
-            </li>)}
-        </ul>
-      </div>
+                {markers.map(marker =>
+                    <li key = {marker.idx}>
+                        <div>{marker.title}</div>
+                    </li>)}
+            </ul>
+        </div>
     )
-  }
+}
 
 export default MapComponent
